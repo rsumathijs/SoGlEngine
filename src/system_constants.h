049bdfc7a566ca8704d12086bf17eec238b80172
Sumi_Swain <rsumathijs@gmail.com>
2016-12-25 05:26:36 -0800

    Add RTTI from Paul for future use

--- src/system_constants.h
+++ src/system_constants.h
@@ -1,3 +1,6 @@
-//screen size
-const int WINDOW_WIDTH = 1280;
-const int WINDOW_HEIGHT = 960;
+namespace SoGlEngine 
+{
+    //screen size
+    const int WINDOW_WIDTH = 1280;
+    const int WINDOW_HEIGHT = 960;
+}
